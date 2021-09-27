6831cf0b752e61487ed97eb4acc3cf237764b649
edusocrates <edu5390@gmail.com>
2021-09-27 20:58:15 -0300

    realizando primeiras configurações para criação da nossa primeira API.

---
+++ src/main/java/br/com/edusocrates/model/Bitcoin.java
@@ -0,0 +1,12 @@
+package br.com.edusocrates.model;
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private long id;
+    private double preco;
+    private String tipo;
+    private LocalDate data;
+
+}

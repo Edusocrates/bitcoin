84578103455bf49d19deac446aa3696e2030530c
edusocrates <edu5390@gmail.com>
2021-09-27 21:27:27 -0300

    realizando ajustes para rodar primeira API

---
+++ src/main/java/br/com/edusocrates/controller/BitcoinController.java
@@ -0,0 +1,27 @@
+package br.com.edusocrates.controller;
+
+import br.com.edusocrates.model.Bitcoin;
+import br.com.edusocrates.service.BitcoinService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/bitcoins")
+public class BitcoinController {
+
+    @Inject
+    @RestClient
+    BitcoinService bitcoinService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Bitcoin> listar(){
+        return bitcoinService.listar();
+    }
+
+}
--- src/main/java/br/com/edusocrates/model/Bitcoin.java
+++ src/main/java/br/com/edusocrates/model/Bitcoin.java
@@ -9,4 +9,35 @@ public class Bitcoin {
     private String tipo;
     private LocalDate data;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
